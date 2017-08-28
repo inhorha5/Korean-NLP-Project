bf0fc25af349bb341d4297a55e869e4282beec05
Edward R <Edward>
2017-08-28 14:06:01 -0700

    continual modularization of code

---
+++ Project.py
@@ -0,0 +1,15 @@
+import scraper
+
+if __name__=="__main__":
+    df = scraper.Update()
+
+
+
+# if __name__ == '__main__':
+#     df = Update()
+
+
+    # Write the collected data to json
+    # df.to_json(json_path)
+
+    # df_month.to_json('data/Data_1_month.json', orient='records', date_format="iso")
